52c38c71b7e5e962f3014905a1494ad1a4bf3598
CACode <54068986+cctvadmin@users.noreply.github.com>
2020-11-02 18:02:10 +0800

    [202011021802]update
    [202011021802]update

--- util/selenium_main.py
+++ util/selenium_main.py
@@ -1,8 +1,13 @@
-# -*- coding: utf-8 -*-
+# -*- coding: UTF-8 -*-
+from time import sleep
+import test
+import getopt
+import sys
 import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ThreadPoolExecutor, as_completed
+import json
 
 thread_pool = ThreadPoolExecutor(max_workers=10)
 
@@ -16,22 +21,19 @@ def run_one(url, js, br=None, sleep_second=0, start_sleep=1):
     time.sleep(start_sleep)
     result = []
     for script in js:
-        js_result = browser.execute_script(
-            f"return {script}")
-        result.append(js_result)
-        if sleep_second != 0:
-            time.sleep(sleep_second)
+        try:
+            js_result = browser.execute_script(
+                f"return {script}")
+            result.append(js_result)
+            if sleep_second != 0:
+                time.sleep(sleep_second)
+        except Exception as e:
+            result.append(str(e))
     return result
 
 
-def run_multiple(urls, js, sleep_second=0, start_sleep=1):
-    """
-    all_task = [executor.submit(get_html, (url)) for url in urls]
-
-    for future in as_completed(all_task):
-        data = future.result()
-        print("in main: get page {}s success".format(data))
-    """
+def run_multiple(urls, js, sleep_second=0, start_sleep=2):
+    # urls, js, sleep_second=0, start_sleep=1
     browser = get_browser()
     result = []
     for url in urls:
@@ -48,24 +50,60 @@ def get_browser():
     """
     chrome_options = Options()
     chrome_options.add_argument('--headless')
+    chrome_options.add_experimental_option(
+        'excludeSwitches', ['enable-automation'])
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--start-maximized')
+    chrome_options.add_argument('--incognito')
+    chrome_options.add_argument("disable-cache")
     browser = webdriver.Chrome(chrome_options=chrome_options)
     return browser
 
 
+def text_save(filename, data):  # filename为写入CSV文件的路径，data为要写入数据列表.
+    file = open(filename, 'w', encoding='utf-8')
+    _result = ''
+    for i in data:
+        for j in i:
+            _result += (str(j).replace('\n', '\t'))+('\t')
+        _result += ('\n')
+    file.write(_result)
+    file.close()
+
+
 if __name__ == '__main__':
-    # document.getElementsByClassName('info-card-item-value')[0].innerText 快递单号
-    # document.getElementsByClassName('info-card-item-value')[1].innerText 快递公司
-    # document.getElementsByClassName('info-card-item-value')[2].innerText 预计送达时间
-    # document.getElementsByClassName('lg-timeline-current')[0].innerText 第一条物流信息，包含送达状态、物流信息、物流更新日期
-    import test
-    _result = run_multiple(
-        urls=test.urls,
-        js=[
-            "document.getElementsByClassName('info-card-item-value')[0].innerText",
-            "document.getElementsByClassName('info-card-item-value')[1].innerText",
-            "document.getElementsByClassName('info-card-item-value')[2].innerText",
-            "document.getElementsByClassName('lg-timeline-current')[0].innerText"
-        ],
-        start_sleep=2
-    )
-    print(_result)
+    # # document.getElementsByClassName('info-card-item-value')[0].innerText
+    # # document.getElementsByClassName('info-card-item-value')[1].innerText
+    # # document.getElementsByClassName('info-card-item-value')[2].innerText
+    # # document.getElementsByClassName('lg-timeline-current')[0].innerText
+
+    # _result = run_multiple(
+    #     urls=test.urls,
+    #     js=test.js,
+    #     start_sleep=2
+    # )
+    # print(_result)
+    # getopt.getopt(sys.argv,)
+    # all_task = run_multiple(urls=test.urls, js=test.js,
+    #                         sleep_second=0, start_sleep=3)
+    # print(all_task)
+    op = open('c:/a.txt')
+    line = op.readline()
+    links = []
+    while line:
+        links.append(line)
+        line = op.readline()
+    links = [str(i).replace('\n', '') for i in links]
+    op = open('c:/b.txt')
+    line = op.readline()
+    js = []
+    while line:
+        js.append(line)
+        line = op.readline()
+    js = [str(i).replace('\n', '') for i in js]
+    _result = run_multiple(urls=links, js=js,
+                           sleep_second=0, start_sleep=1)
+    op.close()
+
+    print(json.dumps(_result))
+    text_save('c:/c.xls', _result)
